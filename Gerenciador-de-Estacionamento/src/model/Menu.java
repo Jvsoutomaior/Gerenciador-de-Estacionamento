780a3bc25d8046b6a62f6daa81d9514ec63f3e4b
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2022-09-02 16:10:29 -0300

    menu com switch

--- Gerenciador-de-Estacionamento/src/model/Menu.java
+++ Gerenciador-de-Estacionamento/src/model/Menu.java
@@ -1,36 +1,180 @@
 package model;
+import javax.swing.JOptionPane;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		int x = 10;
-		while(x != 0) {
-			//cadastro , atualização, pesquisa, remoção
-			//Estacionamento, Evento, Acesso
+		int option = 0;
+		String menu = "";
+		//Estacionamento
+		menu += "1) Adicionar Estacionamento\n";
+		menu += "2) Editar Estacionamento\n";
+		menu += "3) pesquisar Estacionamento\n";
+		menu += "4) Excluir Estacionamento\n";
+		//Evento (so pode existir se exixtir um estacionamento)
+		menu += "5) Adicionar Evento\n";
+		menu += "6) Editar Evento\n";
+		menu += "7) Pesquisar Evento\n";
+		menu += "8) Excluir Evento\n";
+		//Acesso (so pode ser acessado se existir um estacionamento)
+		menu += "9) Adicionar Acesso\n";
+		menu += "10) Editar Acesso\n";
+		menu += "11) Pesquisar Acesso\n";
+		menu += "12) Excluir Acesso\n";
+
+		menu += "13) Encerrar Programa\n";
+
+		while(option != 13) {
+			String strOption = JOptionPane.showInputDialog(menu);
+            option = Integer.parseInt(strOption);
 			
-			//Estacionamento
-			System.out.println("1: Adicionar Estacionamento");
-			System.out.println("2: Editar Estacionamento");
-			System.out.println("3: pesquisar Estacionamento");
-			System.out.println("4: Excluir Estacionamento");
+			switch(option){
+				case 1: {
+
+					break;
+				}
+				case 2: {
+					
+					break;
+				}
+				case 3: {
+					
+					break;
+				}
+				case 4: {
+					
+					break;
+				}
+				case 5: {
+					
+					break;
+				}
+				case 6: {
+					
+					break;
+				}
+				case 7: {
+					
+					break;
+				}
+				case 8: {
+					
+					break;
+				}
+				case 9: {
+					
+					break;
+				}
+				case 10: {
+					
+					break;
+				}
+				case 11: {
+					
+					break;
+				}
+				case 12: {
+					
+					break;
+				}
+				case 13: {
+					JOptionPane.showMessageDialog(null, "Encerrando...");
+					break;
+				}
+				default: 
+                    JOptionPane.showMessageDialog(null, "Entrada invalida!\n");
+			}
 			
+		}
+
+	}
+
+}package model;
+import javax.swing.JOptionPane;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		int option = 0;
+		String menu = "";
+		//Estacionamento
+		menu += "1) Adicionar Estacionamento\n";
+		menu += "2) Editar Estacionamento\n";
+		menu += "3) pesquisar Estacionamento\n";
+		menu += "4) Excluir Estacionamento\n";
+		//Evento (so pode existir se exixtir um estacionamento)
+		menu += "5) Adicionar Evento\n";
+		menu += "6) Editar Evento\n";
+		menu += "7) Pesquisar Evento\n";
+		menu += "8) Excluir Evento\n";
+		//Acesso (so pode ser acessado se existir um estacionamento)
+		menu += "9) Adicionar Acesso\n";
+		menu += "10) Editar Acesso\n";
+		menu += "11) Pesquisar Acesso\n";
+		menu += "12) Excluir Acesso\n";
+
+		menu += "13) Encerrar Programa\n";
+
+		while(option != 13) {
+			String strOption = JOptionPane.showInputDialog(menu);
+            option = Integer.parseInt(strOption);
 			
-			//Evento (so pode existir se exixtir um estacionamento)
-			System.out.println("5: Adicionar Evento");
-			System.out.println("6: Editar Evento");
-			System.out.println("7: pesquisar Evento");
-			System.out.println("8: Excluir Evento");
-			
-			//Acesso (so pode ser acessado se existir um estacionamento)
-			System.out.println("9: Adicionar Acesso");
-			System.out.println("10: Editar Acesso");
-			System.out.println("11: pesquisar Acesso");
-			System.out.println("12: Excluir Acesso");
-			
-			System.out.print("0: Encerrar Programa");
-			
-			
-			
+			switch(option){
+				case 1: {
+
+					break;
+				}
+				case 2: {
+					
+					break;
+				}
+				case 3: {
+					
+					break;
+				}
+				case 4: {
+					
+					break;
+				}
+				case 5: {
+					
+					break;
+				}
+				case 6: {
+					
+					break;
+				}
+				case 7: {
+					
+					break;
+				}
+				case 8: {
+					
+					break;
+				}
+				case 9: {
+					
+					break;
+				}
+				case 10: {
+					
+					break;
+				}
+				case 11: {
+					
+					break;
+				}
+				case 12: {
+					
+					break;
+				}
+				case 13: {
+					JOptionPane.showMessageDialog(null, "Encerrando...");
+					break;
+				}
+				default: 
+                    JOptionPane.showMessageDialog(null, "Entrada invalida!\n");
+			}
 			
 		}
 
